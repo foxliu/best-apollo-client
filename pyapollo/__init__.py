eb669c9a0921d74ee8cdab1f9177b23b9870412f
liusonghui <liusonghui@bestpay.com.cn>
2021-12-16 16:23:45 +0800

    更改说明和一些配置

--- pyapollo/__init__.py
+++ pyapollo/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from .apollo_client import ApolloClient
 
-__version__ = "0.0.1.dev2"
+__version__ = "0.0.2.dev2"
 __author__ = 'foxliu'
 __email__ = 'foxliu2012@gmail.com'
--- setup.py
+++ setup.py
@@ -1,22 +1,28 @@
 # encoding: utf-8
 """
-pyapollo 常用工具包
+#### Apollo client python
+- 启动客户端长连接监听
 
+``` python
+client = ApolloClient(app_id=<appId>, cluster=<clusterName>, config_server_url=<configServerUrl>)
+client.client.add_callback_funcs(<cb_funs>)  # 添加回调函数，回调函数接收一个参数为当前appId的配置
+client.start()
+```
 
+- 获取Apollo的配置
+  ```
+  client.get_value(Key, DefaultValue)
+  ```
 """
 from setuptools import setup, find_packages
 import pyapollo
 
-SHORT = u'pyapollo'
+SHORT = u'best-apollo-client'
 
 setup(
-    name='pyapollo',
+    name='best-apollo-client',
     version=pyapollo.__version__,
     packages=find_packages(),
-    install_requires=[
-        'requests',
-        'pyyaml'
-    ],
     url='https://github.com/foxliu/pyapollo.git',
     author=pyapollo.__author__,
     author_email='foxliu2012@gmail.com',
@@ -27,7 +33,9 @@ setup(
     package_data={'': ['*.py', '*.pyc']},
     zip_safe=False,
     platforms='any',
+    python_requires='>=3.7',
 
     description=SHORT,
     long_description=__doc__,
+    long_description_content_type='text/markdown'
 )
