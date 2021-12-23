649f5110508573d4e9155d9d362081ea37a1362b
liusonghui <liusonghui@bestpay.com.cn>
2021-12-23 09:39:23 +0800

    change version

--- bp_apollo_client/__init__.py
+++ bp_apollo_client/__init__.py
@@ -2,6 +2,6 @@
 from .apollo_client import ApolloClient
 from .apollo_client import ApolloData
 
-__version__ = "0.0.5"
+__version__ = "0.0.6"
 __author__ = 'foxliu'
 __email__ = 'foxliu2012@gmail.com'
